e83f8f21583de1c969f6ca98c268052d3adc4616
jenecilpatac <jenecilpatac@gmail.com>
2023-02-17 12:14:20 -1100

    name updated

--- views/tpl_profile2.php
+++ views/tpl_profile2.php
@@ -14,11 +14,11 @@
                         <div class="col-sm-9 text-secondary">
                             <select name="COURSE" tabindex="-1" class="form-control form-control-user select2-hidden-accessible" aria-hidden="true" >
                                 
-                                <option>Bachelor of Science in Information Technology</option>
-                                <option>Bachelor of Science in Computer Science</option>
-                                <option>Bachelor of Science in Electrical Technology</option>
-                                <option>Bachelor of Science in Electronics Technology</option>
-                                <option>Bachelor of Science in Industrial Technology Major in Food Technology</option>
+                                <option name="COURSE" value="bsit">Bachelor of Science in Information Technology</option>
+                                <option name="COURSE" value="bscs">Bachelor of Science in Computer Science</option>
+                                <option name="COURSE" value="bs_elec">Bachelor of Science in Electrical Technology</option>
+                                <option name="COURSE" value="bs_elex">Bachelor of Science in Electronics Technology</option>
+                                <option name="COURSE" value="bsit_fpsm">Bachelor of Science in Industrial Technology Major in Food Technology</option>
                             </select> 
                         </div>
                         </div>
@@ -29,17 +29,17 @@
                         <div class="col-sm-9 text-secondary">
                             <select name="BATCH" tabindex="-1" class="form-control form-control-user select2-hidden-accessible" aria-hidden="true" >
                                 
-                                <option>2015</option>
-                                <option>2016</option>
-                                <option>2017</option>
-                                <option>2018</option>
-                                <option>2019</option>
-                                <option>2020</option>
-                                <option>2021</option>
-                                <option>2022</option>
-                                <option>2023</option>
-                                <option>2024</option>
-                                <option>2025</option>
+                                <option name="BATCH" value="2015">2015</option>
+                                <option name="BATCH" value="2016">2016</option>
+                                <option name="BATCH" value="2017">2017</option>
+                                <option name="BATCH" value="2018">2018</option>
+                                <option name="BATCH" value="2019">2019</option>
+                                <option name="BATCH" value="2020">2020</option>
+                                <option name="BATCH" value="2021">2021</option>
+                                <option name="BATCH" value="2022">2022</option>
+                                <option name="BATCH" value="2023">2023</option>
+                                <option name="BATCH" value="2024">2024</option>
+                                <option name="BATCH" value="2025">2025</option>
                             </select>       
                         </div>
                         </div>    
@@ -103,19 +103,19 @@
                         
                         <p >2. What made you pursue advance studies?</p>
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="FOR_PROMOTION" id="inlineRadio1" value="option1">
+                            <input class="form-check-input" type="radio" name="PURSUE_ADVANCE_STUDIES" id="inlineRadio1" value="promotion">
                             <label class="form-check-label" for="inlineRadio1">
                                 For Promotion
                             </label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="FOR_PROFDEV" id="inlineRadio2" value="option2">
+                            <input class="form-check-input" type="radio" name="PURSUE_ADVANCE_STUDIES" id="inlineRadio2" value="professional_development">
                             <label class="form-check-label" for="inlineRadio2">
                                 For Professional Development
                             </label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="2_OTHERS" id="inlineRadio3" value="option3">
+                            <input class="form-check-input" type="radio" name="PURSUE_ADVANCE_STUDIES" id="inlineRadio3" value="others">
                             <label class="form-check-label" for="inlineRadio3">
                                 Others, Please Specify
                             </label> 
