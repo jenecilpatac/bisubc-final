201f49bb71b19afa5ff758200dbf1d98adfe9a27
jenecilpatac <jenecilpatac@gmail.com>
2023-02-17 03:21:32 -1100

    footer updated

--- views/footer.php
+++ views/footer.php
@@ -3,13 +3,51 @@
             <!-- End of Main Content -->
 
             <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; BISU - BC Alumni Information System 2023</span>
-                    </div>
+            <div class="container-fluid footer position-relative bg-warning text-white-50 mt-5 py-5 px-4 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="row g-5 py-5">
+            <div class="col-lg-6 pe-lg-5">
+                <a href="index.php" class="navbar-brand">
+                    <h1 class="display-5 text-light">BISU-BC AIS</h1>
+                </a>
+                <p>Aliquyam sed elitr elitr erat sed diam ipsum eirmod eos lorem nonumy. Tempor sea ipsum diam  sed clita dolore eos dolores magna erat dolore sed stet justo et dolor.</p>
+                <p><i class="fa fa-map-marker-alt me-2"></i>Magsija, Balilihan, Bohol</p>
+                <p><i class="fa fa-phone-alt me-2"></i>+639 345 67890</p>
+                <p><i class="fa fa-envelope me-2"></i>bisu_bc@ais.com</p>
+                <div class="d-flex justify-content-start mt-4">
+                    <a class="btn btn-square btn-outline-primary rounded-circle me-2" href="#"><i class="fab fa-twitter"></i></a>
+                    <a class="btn btn-square btn-outline-primary rounded-circle me-2" href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-square btn-outline-primary rounded-circle me-2" href="#"><i class="fab fa-linkedin-in"></i></a>
+                    <a class="btn btn-square btn-outline-primary rounded-circle me-2" href="#"><i class="fab fa-instagram"></i></a>
                 </div>
-            </footer>
+            </div>
+            <div class="col-lg-6 ps-lg-5">
+                <div class="row g-5">
+
+                    <div class="col-sm-6">
+                        <h4 class="text-light mb-4">VISION</h4>
+                        <p>A premier Science and Technology university for the formation of world class and virtuous human resource for sustainable development in Bohol and the Country.</p>
+                    </div>
+                    <div class="col-sm-6">
+                        <h4 class="text-light mb-4">MISSION</h4>
+                        <p>BISU is committed to provide quality higher education in the arts and sciences, as well as in the professional and technological fields; undertake research and development and extension services for the sustainable development of Bohol and the country.
+
+                        </p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid bg-dark text-white border-top border-secondary px-0">
+        <div class="d-flex flex-column flex-md-row justify-content-between">
+            <div class="py-4 px-5 text-center text-md-start">
+                <p class="mb-0">&copy; <a class="text-primary" href="index.php">BISU-BC AIS</a>. All Rights Reserved 2023.</p>
+            </div>
+            <div class="py-4 px-5 bg-secondary footer-shape position-relative text-center text-md-end">
+                
+            </div>
+        </div>
+    </div>
             <!-- End of Footer -->
 
         </div>
